5c82a3fb2017597cb270064cafd56e83f9532430
sontv8 <sontv8@fpt.edu.vn>
2023-03-08 10:37:19 +0700

    day01

---
+++ main.js
@@ -0,0 +1,26 @@
+"use strict";
+export const __esModule = true;
+// const a: number = 10
+// const b: number = 20
+// let user: string;
+// user = 123;
+// user = "123";
+// console.log(a + b);
+// const isAdmin: boolean = true;
+// const arrNumber: number[] = [1, 2, 3]
+// const arrStr: string[] = ["1224234", 'b']
+// const arrMix: Array<number | string | boolean> = [1, "abc", true]
+// console.log(arrMix);
+// const user: { id: number, username: string } = {
+//     id: 1,
+//     username: "datdeptrai"
+// }
+// console.log(user);
+var a = 10;
+var b = 20;
+console.log(a + b);
+function sum(a, b) {
+    var result = a + b;
+    return result;
+}
+sum(10, 20);
---
+++ main.ts
@@ -0,0 +1,43 @@
+export { }
+
+// const a: number = 10
+// const b: number = 20
+
+// let user: string;
+// user = 123;
+// user = "123";
+// console.log(a + b);
+// const isAdmin: boolean = true;
+
+// const arrNumber: number[] = [1, 2, 3]
+// const arrStr: string[] = ["1224234", 'b']
+// const arrMix: Array<number | string | boolean> = [1, "abc", true]
+// console.log(arrMix);
+
+// const user: { id: number, username: string } = {
+//     id: 1,
+//     username: "datdeptrai"
+// }
+// console.log(user);
+const a = 10;
+const b = 20;
+console.log(a + b);
+
+function sum(a: number, b: number): number {
+    let result = a + b
+    return result
+}
+sum(10, 20)
+
+interface IProduct {
+    id?: number, name: string
+}
+
+const project: { id?: number, name: string } = {
+    id: 1,
+    name: "A"
+}
+function add(project: { id?: number, name: string }): { id?: number, name: string } {
+    return project;
+}
+add(project)

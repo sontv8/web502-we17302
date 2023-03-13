858ca44a78b4d1d6d11043fd7ab50aefe99d88f1
sontv8 <sontv8@fpt.edu.vn>
2023-03-13 13:15:21 +0700

    Buổi 4: React - tạo router

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,12 +1,26 @@
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import ProductPage from './pages/Product'
+import ProductDetailPage from './pages/ProductDetail'
 
 function App() {
 
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/">
+          <Route index element={<HomePage />} />
+          <Route path="products">
+            <Route index element={<ProductPage />} />
+            <Route path=":id" element={<ProductDetailPage />} />
+          </Route>
+        </Route>
+        {/* /admin */}
+        {/* 
+          / - HomePage
+          /products - ProductsPage
+          /products/:id - ProductDetailPage
+        */}
       </Routes>
     </div>
   )

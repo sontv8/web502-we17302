299e3efa7aae6b63e80aa6b7ab14a610b1e60212
sontv8 <sontv8@fpt.edu.vn>
2023-03-22 12:36:26 +0700

    Buổi 8: cập nhật thêm mới sản phẩm

--- react/src/pages/admin/AddProduct.jsx
+++ react/src/pages/admin/AddProduct.jsx
@@ -1,23 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const AddProductPage = (props) => {
-    const navigate = useNavigate()
-    const addNewProduct = () => {
-        const newProduct = {
-            "name": document.querySelector("#prdName").value,
-        }
-        try {
-            props.onAdd(newProduct)
-            navigate('/admin/products')
-        } catch (error) {
-            console.log(error);
-        }
+    const navigate = useNavigate() // khởi tạo navigate để điều hướng
+    // const addNewProduct = () => {
+    //     // const newProduct = {
+    //     //     "name": document.querySelector("#prdName").value,
+    //     // }
+    //     // try {
+    //     //     props.onAdd(newProduct)
+    //     //     navigate('/admin/products')
+    //     // } catch (error) {
+    //     //     console.log(error);
+    //     // }
+    // }
+    const [inputValue, setInputValue] = useState({}) // khởi tạo state để lưu giá trị input
+    const onHandleChange = (e) => { // hàm xử lý sự kiện khi thay đổi giá trị input
+        const name = e.target.name; // lấy name của input
+        setInputValue({ ...inputValue, [name]: e.target.value }); // cập nhật giá trị mới cho state
+        // setInputValue({ name: e.target.value });
+    }
+    const onHandleSubmit = (e) => { // hàm xử lý sự kiện khi submit form
+        e.preventDefault(); // ngăn chặn sự kiện submit mặc định của form
+        // console.log(inputValue);
+        props.onAdd(inputValue); // gọi hàm onAdd từ props để thêm mới sản phẩm
+        navigate('/admin/products') // điều hướng đến trang quản lý sản phẩm
     }
     return (
         <div>
-            <form action="" onSubmit={() => addNewProduct()}>
-                <input type="text" placeholder='Product Name' id='prdName' />
+            <form action="" onSubmit={onHandleSubmit}>
+                <input type="text" placeholder='Product Name' id='prdName' onChange={onHandleChange} name='name' />
+                <input type="number" onChange={onHandleChange} name='price' />
                 <button type='submit'>Add New</button>
             </form>
         </div>

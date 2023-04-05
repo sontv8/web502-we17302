3e461845c5ab2632ff8e79d0a16226978854a2ad
sontv8 <sontv8@fpt.edu.vn>
2023-04-05 12:26:53 +0700

    Buổi 14: ant design - update product

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,42 +1,41 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-// import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import ProductPage from './pages/Product'
-import { addProduct, deleteProduct, getAllProduct } from './api/product'
-import ProductDetailPage from './pages/ProductDetail'
-import { IProduct } from './types/product'
+import { useState, useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { addProduct, deleteProduct, getAllProduct, updateProduct } from './api/product'
 import AddProductPage from './pages/admin/AddProduct'
 import ProductManagementPage from './pages/admin/ProductManagement'
-
+import UpdateProductPage from './pages/admin/UpdateProduct'
+import HomePage from './pages/HomePage'
+import ProductPage from './pages/Product'
+import ProductDetailPage from './pages/ProductDetail'
+import { IProduct } from './types/product'
 
 function App() {
-  const [products, setProduct] = useState<IProduct[]>([])
+  const [products, setProducts] = useState<IProduct[]>([])
   useEffect(() => {
-    getAllProduct().then(({ data }) => setProduct(data))
+    getAllProduct().then(({ data }) => setProducts(data))
   }, [])
   const onHandleRemove = (id: number) => {
-    deleteProduct(id)
+    deleteProduct(id).then(() => setProducts(products.filter((item: IProduct) => item.id !== id)))
   }
-  const onHandleAdd = (product) => {
-    addProduct(product)
+  const onHandleAdd = (product: IProduct) => {
+    addProduct(product).then(() => getAllProduct().then(({ data }) => setProducts(data)))
+  }
+  const onHandleUpdate = (product: IProduct) => {
+    updateProduct(product).then(() => getAllProduct().then(({ data }) => setProducts(data)))
   }
   return (
     <div className="App">
       <Routes>
         <Route path='/'>
           <Route index element={<HomePage />} />
-          <Route path='products' >
-            <Route index element={<ProductPage products={products} onRemove={onHandleRemove} />} />
-            <Route path=':id' element={<ProductDetailPage />} />
-          </Route>
+          <Route path='products' element={<ProductPage products={products} onRemove={onHandleRemove} />} />
+          <Route path='products/:id' element={<ProductDetailPage />} />
         </Route>
         <Route path='/admin'>
           <Route path='products'>
             <Route index element={<ProductManagementPage products={products} onRemove={onHandleRemove} />} />
             <Route path='add' element={<AddProductPage onAdd={onHandleAdd} />} />
+            <Route path=':id/update' element={<UpdateProductPage onUpdate={onHandleUpdate} products={products} />} />
           </Route>
         </Route>
       </Routes>
